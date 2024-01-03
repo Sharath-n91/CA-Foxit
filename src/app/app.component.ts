7e64977d6384890e75372d9a6f857114a614c464
Sharath-n91 <152499238+Sharath-n91@users.noreply.github.com>
2024-01-03 18:03:11 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { CustomModuleDialogComponent } from './shared/custom-module-dialog/custom-module-dialog.component';
+import { PdfViewerComponent } from './shared/pdf-viewer/pdf-viewer.component';
+import { SimpleDialogComponent } from './shared/simple-dialog/simple-dialog.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  constructor(
+  private matDialog: MatDialog,
+  ){}
+
+  openFoxitDialog(){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.panelClass = 'confirmaton-dialog-foxit';
+    dialogConfig.autoFocus = true;
+    let dialogInstance: any;
+
+    dialogInstance = this.matDialog.open(PdfViewerComponent, dialogConfig);
+  }
+  openFoxitDialogCustom(){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    let dialogInstance: any;
+
+    dialogInstance = this.matDialog.open(CustomModuleDialogComponent, dialogConfig);
+  }
+  openSimpleDialog(){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    let dialogInstance: any;
+
+    dialogInstance = this.matDialog.open(SimpleDialogComponent, dialogConfig);
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+
+import { AppRoutingModule } from './app-routing.module';
+import { PdfViewerComponent } from '../app/shared/pdf-viewer/pdf-viewer.component';
+import { HomeComponent } from './shared/home/home.component';
+import { CopysheetComponent } from './shared/copysheet/copysheet.component';
+import { ContactsComponent } from './shared/contacts/contacts.component';
+import { AboutComponent } from './shared/about/about.component';
+import { SimpleDialogComponent } from './shared/simple-dialog/simple-dialog.component';
+import { CustomModuleDialogComponent } from './shared/custom-module-dialog/custom-module-dialog.component';
+import { CopysheetCustomComponent } from './shared/copysheet-custom/copysheet-custom.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PdfViewerComponent,
+    HomeComponent,
+    CopysheetComponent,
+    ContactsComponent,
+    AboutComponent,
+    SimpleDialogComponent,
+    CustomModuleDialogComponent,
+    CopysheetCustomComponent
+  ],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDialogModule
+  ],
+  entryComponents:[PdfViewerComponent],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
